c5b8249cce1a3b52b562799368ab3dd816ac6a6b
Shailesh Chanderiya <shailesh.chanderiya85@gmail.com>
2026-03-22 10:07:35 +0530

    new changes

--- main.tf
+++ main.tf
@@ -30,46 +30,43 @@ resource "azurerm_resource_group" "practicesaz305" {
   location = var.location
 }
 
-resource "azurerm_virtual_network" "exampracticesaz305ple" {
+
+resource "azurerm_virtual_network" "practicesaz305" {
   name                = "practicesaz305-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.practicesaz305.location
+  resource_group_name = azurerm_resource_group.practicesaz305.name
 }
 
-resource "azurerm_subnet" "practicesaz305subnet" {
+resource "azurerm_subnet" "practicesaz305" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.practicesaz305.name
+  virtual_network_name = azurerm_virtual_network.practicesaz305.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "practicesaz305nic" {
+resource "azurerm_network_interface" "practicesaz305" {
   name                = "practicesaz305-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.practicesaz305.location
+  resource_group_name = azurerm_resource_group.practicesaz305.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.practicesaz305.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "practicesaz305vm" {
+resource "azurerm_windows_virtual_machine" "practicesaz305" {
   name                = "practicesaz305-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.practicesaz305.name
+  location            = azurerm_resource_group.practicesaz305.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.practicesaz305.id,
   ]
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
 
   os_disk {
     caching              = "ReadWrite"
@@ -77,9 +74,9 @@ resource "azurerm_linux_virtual_machine" "practicesaz305vm" {
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
     version   = "latest"
   }
 }
